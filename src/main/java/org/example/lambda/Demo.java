996bc1618f894d1eb63fa56124579842937f0c92
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2024-03-12 00:56:47 +0530

    predicate Added

--- src/main/java/org/example/lambda/Demo.java
+++ src/main/java/org/example/lambda/Demo.java
@@ -1,10 +1,21 @@
 package org.example.lambda;
 
+import java.util.function.Predicate;
+
 public class Demo {
     public static void main(String[] args) {
         DivisibleByOneInterface divisibleByOneInterface = p -> p % 2 == 0;
         System.out.println(divisibleByOneInterface.isDivisibleByTwo(5));
         System.out.println(divisibleByOneInterface.isDivisibleByTwo(4));
+
+
+        DivisibleByOneInterface divisibleBy5 = p -> p%5 ==0;
+        System.out.println(divisibleBy5.isDivisibleByTwo(5));
+        System.out.println(divisibleBy5.isDivisibleByTwo(4));
+
         // Test with an even number
+
+        Predicate <Integer> divisibleByTwo = p -> p % 2 == 0;
+        System.out.println(divisibleByTwo.test(5));
     }
 }
